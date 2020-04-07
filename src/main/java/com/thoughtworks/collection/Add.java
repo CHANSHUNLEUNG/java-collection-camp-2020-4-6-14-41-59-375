97f0ac989dc186dd2b4a206a58db8831e282b133
CHANSHUNLEUNG <33828494+CHANSHUNLEUNG@users.noreply.github.com>
2020-04-07 11:52:50 +0800

    pass add test cases.

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,36 +2,67 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Add {
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().mapToInt(element -> {
+            return (element % 2 != 0) ? element * 3 + 5 : 0;
+        }).sum();
     }
 
     public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        List<Integer> oddListDesc = arrayList.stream().filter(element -> element % 2 != 0).
+                sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        List<Integer> evenListAsc = arrayList.stream().filter(element -> element % 2 == 0).
+                sorted().collect(Collectors.toList());
+        List<Integer> resultList = new ArrayList<Integer>();
+        resultList.addAll(evenListAsc);
+        resultList.addAll(oddListDesc);
+        return resultList;
     }
 
 
     public boolean isIncludedInEven(List<Integer> arrayList, Integer specialElement) {
-        throw new NotImplementedException();
+        List<Integer> evenList = arrayList.stream().filter(element -> element % 2 == 0).collect(Collectors.toList());
+        return evenList.contains(specialElement);
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().map(element -> {
+            return (element % 2 != 0) ? element * 3 + 2 : element;
+        }).collect(Collectors.toList());
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().mapToInt(element -> element * 3 + 2).sum();
     }
 
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int sum = 0;
+        int smallerBorder = (leftBorder < rightBorder) ? leftBorder : rightBorder;
+        int biggerBorder = (leftBorder < rightBorder) ? rightBorder : leftBorder;
+        for(int number = smallerBorder; number <= biggerBorder; number++){
+            if(number % 2 == 0){
+                sum += number;
+            }
+        }
+        return sum;
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int sum = 0;
+        int smallerBorder = (leftBorder < rightBorder) ? leftBorder : rightBorder;
+        int biggerBorder = (leftBorder < rightBorder) ? rightBorder : leftBorder;
+        for(int number = smallerBorder; number <= biggerBorder; number++){
+            if(number % 2 != 0){
+                sum += number;
+            }
+        }
+        return sum;
     }
 
 }
